583e5bfe1f8d36d2599b67a795d87fa3a674505a
Erick Silva <erick.silva@firstdecision.com.br>
2021-01-21 19:37:55 -0300

    feat: basically, working

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,48 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult, '');
-}
+const add = () => {
+  const userNumber = getUserInput();
+  const initialRes = currentResult;
+  currentResult = currentResult + userNumber;
+  createLog('+', initialRes, userNumber);
+};
+
+const substract = () => {
+  const userNumber = getUserInput();
+  const initialRes = currentResult;
+  currentResult = currentResult - userNumber;
+  createLog('-', initialRes, userNumber);
+};
+
+const multiply = () => {
+  const userNumber = getUserInput();
+  const initialRes = currentResult;
+  currentResult = currentResult * userNumber;
+  createLog('*', initialRes, userNumber);
+};
+
+const divide = () => {
+  const userNumber = getUserInput();
+  const initialRes = currentResult;
+  currentResult = currentResult / userNumber;
+  createLog('/', initialRes, userNumber);
+};
+
+// AUX FUNCS //
+
+const getUserInput = () => {
+  return parseInt(userInput.value);
+};
+
+const createLog = (op, resB4Calc, numCalc) => {
+  const calcDesc = `${resB4Calc} ${op} ${numCalc}`;
+  outputResult(currentResult, calcDesc);
+};
+
+// AUX FUNCS //
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', substract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);

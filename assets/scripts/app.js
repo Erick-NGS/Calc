af5a1648ed82eed70b78d80cf5c86dc1e985187f
Erick Silva <erick.silva@firstdecision.com.br>
2021-01-22 07:52:34 -0300

    feat: end of module

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,32 +1,37 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+const logEntries = [];
 
 const add = () => {
   const userNumber = getUserInput();
   const initialRes = currentResult;
-  currentResult = currentResult + userNumber;
+  currentResult += userNumber;
   createLog('+', initialRes, userNumber);
+  writeToLog('ADDITION', initialRes, userNumber, currentResult);
 };
 
 const substract = () => {
   const userNumber = getUserInput();
   const initialRes = currentResult;
-  currentResult = currentResult - userNumber;
+  currentResult -= userNumber;
   createLog('-', initialRes, userNumber);
+  writeToLog('SUBSTRACTION', initialRes, userNumber, currentResult);
 };
 
 const multiply = () => {
   const userNumber = getUserInput();
   const initialRes = currentResult;
-  currentResult = currentResult * userNumber;
+  currentResult *= userNumber;
   createLog('*', initialRes, userNumber);
+  writeToLog('MULTIPLICATION', initialRes, userNumber, currentResult);
 };
 
 const divide = () => {
   const userNumber = getUserInput();
   const initialRes = currentResult;
-  currentResult = currentResult / userNumber;
+  currentResult /= userNumber;
   createLog('/', initialRes, userNumber);
+  writeToLog('DIVISION', initialRes, userNumber, currentResult);
 };
 
 // AUX FUNCS //
@@ -38,6 +43,17 @@ const getUserInput = () => {
 const createLog = (op, resB4Calc, numCalc) => {
   const calcDesc = `${resB4Calc} ${op} ${numCalc}`;
   outputResult(currentResult, calcDesc);
+};
+
+const writeToLog = (op, prevRes, opNumber, newRes) => {
+  const logEntry = {
+    op: op,
+    firstNum: prevRes,
+    secNum: opNumber,
+    result: newRes,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 };
 
 // AUX FUNCS //
